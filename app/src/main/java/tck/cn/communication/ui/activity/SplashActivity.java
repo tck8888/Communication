b01f114b7e1abd666a5962992f4f418c2aa5490b
tck8888 <380054728@qq.com>
2016-10-15 11:09:15 +0800

    Initial commit

---
+++ app/src/main/java/tck/cn/communication/ui/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package tck.cn.communication.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tck.cn.communication.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
